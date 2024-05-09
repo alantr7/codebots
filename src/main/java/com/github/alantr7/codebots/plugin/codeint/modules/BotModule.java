faea1716afb8f33e8c996bc429b1ab63e008efa2
alantr7 <alan.trumic066@gmail.com>
2024-05-09 20:02:58 +0200

    bot.getBlock()

--- src/main/java/com/github/alantr7/codebots/plugin/codeint/modules/BotModule.java
+++ src/main/java/com/github/alantr7/codebots/plugin/codeint/modules/BotModule.java
@@ -1,5 +1,7 @@
 package com.github.alantr7.codebots.plugin.codeint.modules;
 
+import com.github.alantr7.codebots.api.bot.CodeBot;
+import com.github.alantr7.codebots.api.bot.Direction;
 import com.github.alantr7.codebots.language.runtime.Program;
 import com.github.alantr7.codebots.language.runtime.modules.NativeModule;
 import com.github.alantr7.codebots.plugin.codeint.functions.MoveFunction;
@@ -19,6 +21,16 @@ public class BotModule extends NativeModule {
         getRootScope().setFunction("moveLeft", new MoveFunction(program, "moveLeft"));
 
         getRootScope().setFunction("rotateRight", new RotateFunction(program, "rotateRight"));
+
+        registerFunction("getBlock", args -> {
+            var bot = (CodeBot) program.getExtra("bot");
+            var input = (String) args[0];
+
+            var direction = input.equals("front") ? bot.getDirection().toVector() :
+                    input.equals("back") ? bot.getDirection().toVector().multiply(-1) : Direction.toDirection(input).toVector();
+
+            return bot.getLocation().add(direction).getBlock().getType().name().toLowerCase();
+        });
     }
 
 }
