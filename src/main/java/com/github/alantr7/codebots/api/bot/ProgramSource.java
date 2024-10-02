f7fa03c5be3b7f4f1248e47ef54516214d89909a
alantr7 <alan.trumic066@gmail.com>
2024-10-02 11:56:15 +0200

    added a shortcut for creating editors from ProgramSource

--- src/main/java/com/github/alantr7/codebots/api/bot/ProgramSource.java
+++ src/main/java/com/github/alantr7/codebots/api/bot/ProgramSource.java
@@ -1,10 +1,15 @@
 package com.github.alantr7.codebots.api.bot;
 
+import com.github.alantr7.codebots.plugin.CodeBotsPlugin;
+import com.github.alantr7.codebots.plugin.editor.CodeEditorClient;
+import com.github.alantr7.codebots.plugin.editor.EditorSession;
 import com.github.alantr7.codebots.plugin.program.ItemFactory;
 import lombok.Getter;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.concurrent.CompletableFuture;
 
 @Getter
 public class ProgramSource {
@@ -26,6 +31,15 @@ public class ProgramSource {
 
     public ItemStack toItem() {
         return ItemFactory.createProgramItem(name, code);
+    }
+
+    public CompletableFuture<EditorSession> createEditor() {
+        try {
+            var bytes = Files.readAllBytes(source.toPath());
+            return CodeBotsPlugin.inst().getSingleton(CodeEditorClient.class).createSession(bytes);
+        } catch (Exception e) {
+            return CompletableFuture.failedFuture(e);
+        }
     }
 
 }
