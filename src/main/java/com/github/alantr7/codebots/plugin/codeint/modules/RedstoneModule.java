6697163ea83d7f33d214017a52e3ccbab0aa1a99
alantr7 <alan.trumic066@gmail.com>
2026-03-09 19:22:37 +0100

    chore: upgraded redstone module to cbs-lang

--- src/main/java/com/github/alantr7/codebots/plugin/codeint/modules/RedstoneModule.java
+++ src/main/java/com/github/alantr7/codebots/plugin/codeint/modules/RedstoneModule.java
@@ -2,6 +2,11 @@ package com.github.alantr7.codebots.plugin.codeint.modules;
 
 import com.github.alantr7.codebots.api.bot.CodeBot;
 import com.github.alantr7.codebots.api.redstone.RedstoneTransmitter;
+import com.github.alantr7.codebots.cbslang.exceptions.ExecutionException;
+import com.github.alantr7.codebots.cbslang.low.runtime.memory.Data;
+import com.github.alantr7.codebots.cbslang.low.runtime.memory.DataType;
+import com.github.alantr7.codebots.cbslang.low.runtime.modules.Context;
+import com.github.alantr7.codebots.cbslang.low.runtime.modules.ExternalFunction;
 import com.github.alantr7.codebots.cbslang.low.runtime.modules.Module;
 import com.github.alantr7.codebots.plugin.CodeBotsPlugin;
 import com.github.alantr7.codebots.plugin.data.TransmitterManager;
@@ -15,25 +20,29 @@ public class RedstoneModule extends Module {
 
     @Override
     public void setup() {
-//        registerFunction("getSignal", args -> {
-//            Assertions.expectArguments(args, Integer.class, Integer.class, Integer.class);
-//            int x = (int) args[0];
-//            int y = (int) args[1];
-//            int z = (int) args[2];
-//
-//            CodeBot bot = (CodeBot) program.getExtra("bot");
-//
-//            Location location = new Location(bot.getLocation().getWorld(), x, y, z);
-//            RedstoneTransmitter transmitter = CodeBotsPlugin.inst().getSingleton(TransmitterManager.class)
-//              .getTransmitter(location);
-//
-//            return transmitter == null ? 0 : transmitter.getPowerAt(bot.getLocation());
-//        });
-//
-//        registerFunction("getInput", args -> {
-//            CodeBot bot = (CodeBot) program.getExtra("bot");
-//            return bot.getLocation().getBlock().getBlockPower();
-//        });
+        registerFunction("get_signal", new ExternalFunction(this, "get_signal", DataType.INT, DataType.INT, DataType.INT, DataType.INT) {
+            @Override
+            public Data handle(Context context) throws ExecutionException {
+                int x = context.getArguments()[0].getValueAs(DataType.INT);
+                int y = context.getArguments()[1].getValueAs(DataType.INT);
+                int z = context.getArguments()[2].getValueAs(DataType.INT);
+
+                CodeBot bot = (CodeBot) context.getProgram().getExtra("bot");
+                Location location = new Location(bot.getLocation().getWorld(), x, y, z);
+                RedstoneTransmitter transmitter = CodeBotsPlugin.inst().getSingleton(TransmitterManager.class)
+                  .getTransmitter(location);
+
+                return new Data(DataType.INT, transmitter == null ? 0 : transmitter.getPowerAt(bot.getLocation()));
+            }
+        });
+
+        registerFunction("get_input", new ExternalFunction(this, "get_input", DataType.INT) {
+            @Override
+            public Data handle(Context context) throws ExecutionException {
+                CodeBot bot = (CodeBot) context.getProgram().getExtra("bot");
+                return new Data(DataType.INT, bot.getLocation().getBlock().getBlockPower());
+            }
+        });
     }
 
 }
