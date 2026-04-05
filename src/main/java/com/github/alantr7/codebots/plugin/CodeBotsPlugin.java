8088f7e8d4f2e2ff0f1d7f84e303e9959a40e865
alantr7 <alan.trumic066@gmail.com>
2026-04-05 19:01:07 +0200

    feat: added bstats metrics

--- src/main/java/com/github/alantr7/codebots/plugin/CodeBotsPlugin.java
+++ src/main/java/com/github/alantr7/codebots/plugin/CodeBotsPlugin.java
@@ -8,6 +8,7 @@ import com.github.alantr7.codebots.cbslang.low.runtime.modules.ModuleRepository;
 import com.github.alantr7.codebots.plugin.codeint.modules.*;
 import com.github.alantr7.codebots.plugin.data.ProgramRegistry;
 import com.github.alantr7.codebots.world.BotsWorldManager;
+import org.bstats.bukkit.Metrics;
 
 @JavaPlugin(name = "CodeBots", version = "0.8.0")
 @Relocations(@Relocate(from = "com.github.alantr7.bukkitplugin", to = "com.github.alantr7.codebots.bpf"))
@@ -16,6 +17,8 @@ public class CodeBotsPlugin extends BukkitPlugin {
     static CodeBotsPlugin instance;
 
     static ModuleRepository MODULE_REPOSITORY = new ModuleRepository();
+
+    private Metrics metrics;
 
     @Override
     protected void onPluginEnable() {
@@ -26,6 +29,8 @@ public class CodeBotsPlugin extends BukkitPlugin {
         MODULE_REPOSITORY.registerModule(new MonitorModule());
         MODULE_REPOSITORY.registerModule(new RedstoneModule());
         MODULE_REPOSITORY.registerModule(new MemoryModule());
+
+        metrics = new Metrics(this, 28911);
     }
 
     @Override
