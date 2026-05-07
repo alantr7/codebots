c6589bf7a5c60d4a39e2251f624a40bed3238b74
alantr7 <alan.trumic066@gmail.com>
2026-05-07 11:46:47 +0200

    feat: added line number to parse error message

--- src/main/java/com/github/alantr7/codebots/plugin/gui/BotProgramsGUI.java
+++ src/main/java/com/github/alantr7/codebots/plugin/gui/BotProgramsGUI.java
@@ -176,7 +176,7 @@ public class BotProgramsGUI extends GUI {
                         } catch (ParserException e) {
                             ((CraftCodeBot) bot).setError(new ProgramError(ProgramError.ErrorLocation.PARSER, e.getMessage()));
                             getPlayer().sendMessage("§cThere was an error while loading the program.");
-                            getPlayer().sendMessage("§4" + e.getMessage());
+                            getPlayer().sendMessage("§cLine #" + e.getLine() + ": §4" + e.getMessage());
 
                             e.printStackTrace();
                         } catch (Exception e) {
